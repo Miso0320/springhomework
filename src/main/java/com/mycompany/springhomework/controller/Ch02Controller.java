9c57b14fb89b3f97b36b3f5c60a1ca6edb52fc51
Miso0320 <[miso8493@hanmail.net]>
2023-07-20 18:50:58 +0900

    230720 commit

--- src/main/java/com/mycompany/springhomework/controller/Ch02Controller.java
+++ src/main/java/com/mycompany/springhomework/controller/Ch02Controller.java
@@ -1,7 +1,13 @@
 package com.mycompany.springhomework.controller;
 
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONObject;
@@ -13,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mycompany.springhomework.dto.Ch02FileInfo;
+import com.mycompany.springhomework.interceptor.Auth;
+import com.mycompany.springhomework.interceptor.Auth.Role;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -119,5 +127,44 @@ public class Ch02Controller {
 		Ch02FileInfo fileInfo = new Ch02FileInfo();
 		fileInfo.setFileName("photo7.jpg");
 		return fileInfo;
+	}
+	
+	@GetMapping("/fileDownload")
+	public void fileDownload(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String fileName = "photo1.jpg";
+		String filePath = "/resources/images/photo/" + fileName;
+		filePath = request.getServletContext().getRealPath(filePath);
+		log.info("filePath : " + filePath);	
+		
+		// 응답 헤드에 Content-Type 추가
+		String mimeType = request.getServletContext().getMimeType(filePath);
+		response.setContentType(mimeType);	//image/jpeg, image/png
+		
+		// 응답 헤드에 한글 이름의 파일명을 ISO-8859-1 문자셋으로 인코딩해서 추가
+		String userAgent = request.getHeader("User-Agent");
+		if(userAgent.contains("Trident") || userAgent.contains("MSIE")) {
+			// IE
+			fileName = URLEncoder.encode(fileName, "UTF-8");
+			log.info("IE : " + fileName);
+			
+		} else {
+			// Chrome, Edge, FireFox, Safari
+			fileName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+			log.info("Chrome : " + filePath);
+		}
+		
+		// 응답 본문에 파일 데이터 싣기
+		OutputStream os = response.getOutputStream();	// 바이너리 데이터
+		Path path = Paths.get(filePath);
+		Files.copy(path, os);
+		os.flush();
+		os.close();
+	}
+	
+	@RequestMapping("/filterAndInterceptor")
+	@Auth(Role.ADMIN)
+	public String adminMethod() {
+		log.info("실행");
+		return "ch02/adminPage";
 	}
 }
